a905d0daf18d0caf749c3250197304d54b03143d
tianx <xueqing.tian@agilysys.com>
2021-01-30 17:17:58 -0500

    after log user

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from './common/form'
 import Joi from 'joi-browser'
+import * as userService from '../services/userService';
 
 class RegisterForm extends Form {
     state = {
@@ -14,10 +15,20 @@ class RegisterForm extends Form {
         name: Joi.string().required().label('Password')
     }
 
-    doSubmit = () => {
-        // call the server
-        console.log('submitted')
+    doSubmit = async () => {
+        try {
+            const response = await userService.register(this.state.data);
+            localStorage.setItem('token', response.headers['x-auth-token'])
+            this.props.history.push('/')
+        } catch (ex) {
+            if (ex.response && ex.response.status === 400) {
+                const errors = {...this.state.errors};
+                errors.username = ex.response.data;
+                this.setState({errors});
+            }
+        }
     }
+        
 
     render() { 
         return (
